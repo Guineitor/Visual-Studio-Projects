e6ee78b7958513aba70a9020ff8f724adf9c2ff0
Guilherme de Araujo Sousa <guineitor.traps236@gmail.com>
2016-03-07 15:10:16 -0300

    update in models

---
+++ Affogato/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Affogato.Models.Account
+{
+    public class User
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Pass { get; set; }
+    }
+}
