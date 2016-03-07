8bdc4384bda735b253bda3e1862f8a12e598f095
Guilherme de Araujo Sousa <guineitor.traps236@gmail.com>
2016-03-07 15:21:26 -0300

    initial commit

---
+++ Affogato/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Affogato.Startup))]
+namespace Affogato
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
